d84f6105b93f50d1820a5bcb4113c1f1600e6c37
Ian Hooper <ianhooper14@gmail.com>
2020-03-19 22:44:47 -0400

    updated for new heroku push

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "",
   database: "burgers_db"
 });
-  }
+  };
 
 // Make connection.
 connection.connect(function(err) {
