92842889215d6fc6db82cd0e49f54ec9ce559495
Ian Hooper <ianhooper14@gmail.com>
2020-04-12 20:55:04 -0400

    updated styling

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,11 @@ if (process.env.JAWSDB_URL) {
   } else {
   connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
 });
-  }
+  };
 
 // Make connection.
 connection.connect(function(err) {
